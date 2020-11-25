12d73455b81a920ae8f8f3d37328b9ee20de9fc2
李政毅 <lizhengyi_m@163.com>
2020-11-25 10:04:52 +0800

    inquirer

--- bin/nm2.js
+++ bin/nm2.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
 const {message} = require('../lib/index.js')
+const inquirer = require('inquirer')
+
 console.log('daxiao hahaha')
 console.log(message)
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Project name?'
+  }
+])
+.then(anwsers => {
+  console.log(anwsers)
+})
